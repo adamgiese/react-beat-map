01f68c6a9561d05550ece509ff3f205ce3bc2e1f
Adam Giese <adam.giese@gmail.com>
2017-07-06 16:07:52 -0500

    Add additional scales

--- src/js/utils/scales.js
+++ src/js/utils/scales.js
@@ -29,6 +29,9 @@ function scaleFromPattern(pattern, root = 'C', initialOctave = 3, numberOfOctave
   for (i; i <= numberOfOctaves * 12; i += pattern[(scale.length - 1) % pattern.length]) {
     const noteName = noteNames[(i + initialIndex) % 12];
     const octave = initialOctave + Math.floor((i + initialIndex) / 12);
+    if (octave === initialOctave) {
+      // console.log(noteName);
+    }
     scale.push(note(noteName, octave));
   }
   return scale;
@@ -36,22 +39,52 @@ function scaleFromPattern(pattern, root = 'C', initialOctave = 3, numberOfOctave
 
 function scales(scaleName, root = 'C', initialOctave = 3, numberOfOctaves = 3) {
   switch (scaleName) {
-    case 'whole':
-      return scaleFromPattern([2], root, initialOctave, numberOfOctaves);
-    case 'chromatic':
-      return scaleFromPattern([1], root, initialOctave, numberOfOctaves);
-    case 'major':
-      return scaleFromPattern([2, 2, 1, 2, 2, 2, 1], root, initialOctave, numberOfOctaves);
-    case 'minor':
-      return scaleFromPattern([2, 1, 2, 2, 1, 2, 2], root, initialOctave, numberOfOctaves);
+    // modes
     case 'lydian':
       return scaleFromPattern([2, 2, 2, 1, 2, 2, 1], root, initialOctave, numberOfOctaves);
+    case 'major':
+    case 'ionian':
+      return scaleFromPattern([2, 2, 1, 2, 2, 2, 1], root, initialOctave, numberOfOctaves);
     case 'mixolydian':
       return scaleFromPattern([2, 2, 1, 2, 2, 1, 2], root, initialOctave, numberOfOctaves);
-    case 'pentatonic':
-      return scaleFromPattern([2, 2, 3, 2, 4], root, initialOctave, numberOfOctaves);
+    case 'dorian':
+      return scaleFromPattern([2, 1, 2, 2, 2, 1, 2], root, initialOctave, numberOfOctaves);
+    case 'minor':
+    case 'aeolian':
+      return scaleFromPattern([2, 1, 2, 2, 1, 2, 2], root, initialOctave, numberOfOctaves);
+    case 'phrygian':
+      return scaleFromPattern([1, 2, 2, 2, 1, 2, 2], root, initialOctave, numberOfOctaves);
+    case 'locrian':
+      return scaleFromPattern([1, 2, 2, 1, 2, 2, 2], root, initialOctave, numberOfOctaves);
+
+    // limited transposition
+    case 'whole':
+    case 'messian-first':
+      return scaleFromPattern([2], root, initialOctave, numberOfOctaves);
+    case 'messian-second':
     case 'octatonic':
       return scaleFromPattern([1, 2], root, initialOctave, numberOfOctaves);
+    case 'messian-third':
+      return scaleFromPattern([2, 1, 1], root, initialOctave, numberOfOctaves);
+    case 'messian-fourth':
+      return scaleFromPattern([1, 1, 3, 1], root, initialOctave, numberOfOctaves);
+    case 'messian-fifth':
+      return scaleFromPattern([1, 4, 1], root, initialOctave, numberOfOctaves);
+    case 'messian-sixth':
+      return scaleFromPattern([2, 2, 1, 1], root, initialOctave, numberOfOctaves);
+    case 'messian-seventh':
+      return scaleFromPattern([1, 1, 1, 2, 1], root, initialOctave, numberOfOctaves);
+
+    // exotic
+    case 'pentatonic':
+      return scaleFromPattern([2, 2, 3, 2, 4], root, initialOctave, numberOfOctaves);
+    case 'japanese':
+      return scaleFromPattern([2, 1, 4, 1, 4], root, initialOctave, numberOfOctaves);
+    case 'phrygian-dominant':
+      return scaleFromPattern([1, 3, 1, 2, 1, 2, 2], root, initialOctave, numberOfOctaves);
+    // other
+    case 'chromatic':
+      return scaleFromPattern([1], root, initialOctave, numberOfOctaves);
     default: // return pentatonic if no scale found
       return scaleFromPattern([2, 2, 3, 2, 4], root, initialOctave, numberOfOctaves);
   }
